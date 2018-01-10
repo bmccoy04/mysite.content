d2f1e6f4103d178c16d9e8299624560ace1b1043
bmccoy04 <bryanmccoy04@gmail.com>
2018-01-09 20:32:21 -0600

    intial commit

---
+++ src/Components/PortraitCircle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './PortraitCircle.css';
+
+const PortraitCircle = (imageUrl) => (
+    <div className = "circle-conatiner">
+        <div className = "circleBase type1">
+        </div>
+    </div>
+);
+
+export default PortraitCircle;
---
+++ src/Containers/SideNav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './SideNav.css';
+import PortraitCircle from '../Components/PortraitCircle';
+
+const SideNav = () => (
+    <div className="sideNav-outer-container">
+       <div className="sideNav-inner-nav">
+           <PortraitCircle></PortraitCircle>
+           <br />
+           <ul>
+               <li><a href="#">ABOUT</a></li>
+               <li><a href="#">EXPERIENCE</a></li>
+               <li><a href="#">EDUCATION</a></li>
+               <li><a href="#">SKILLS</a></li>
+               <li><a href="#">INTEREST</a></li>
+               <li><a href="#">OTHER</a></li>
+               
+           </ul>
+       </div> 
+    </div>
+);
+
+export default SideNav;
