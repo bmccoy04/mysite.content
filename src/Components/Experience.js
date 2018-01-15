ae6075216a86a5ff572425336d0d2dcaddbbee85
bmccoy04 <bryanmccoy04@gmail.com>
2018-01-15 16:59:35 -0600

    added experience to the main page

---
+++ src/Components/Experience.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../Styles/Experience.css'
+
+const Experience = () => (
+    <div className="experience-main-container">
+        <div>
+            My Job
+        </div>
+        <div>
+            I write code... Some good, some not.
+        </div>
+        <div>
+            * Some Circles that link to my stuff *
+        </div>
+    </div>
+);
+
+export default Experience;
--- src/Containers/MainContent.js
+++ src/Containers/MainContent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../Styles/MainContent.css'
 import About from '../Components/About'
+import Experience from '../Components/Experience'
 
 const MainContent = () => (
     <div className="maincontent-maincontainer">
         <About></About>
+        <Experience></Experience>
     </div>
 );
 
