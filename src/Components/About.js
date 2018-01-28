ef83494aa90a1745c169ae44f5c0b11e17d996c8
bmccoy04 <bryanmccoy04@gmail.com>
2018-01-28 17:40:19 -0600

    style stuff

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,6 +9,9 @@ const About = () => (
                 
                 <span className="about-mccoy">MCCOY</span>
             </div>
+            <div className="blue-font about-address-container">
+                <span className="yellow-font">DENHAM SPRINGS LA </span>- BRYANMCCOY04@GMAIL.COM 
+            </div>
             <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
